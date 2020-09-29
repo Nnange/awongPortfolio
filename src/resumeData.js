d436bf584337e70e22a7c2d48231c650c8ae03c1
Awong Nnange <awong.nnange@yahoo.com>
2020-09-30 00:09:37 +0200

    Modified the resumeData file

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,60 +1,92 @@
 let resumeData = {
     "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
+    "name": "Nnange Awong ",
+    "role": "Fullstack Developer and Data Scientist",
     "linkedinId":"Your LinkedIn Id",
     "skypeid": "Your skypeid",
     "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
     "socialLinks":[
         {
           "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+          "url":"https://www.linkedin.com/in/awong-nnange/",
           "className":"fa fa-linkedin"
         },
         {
           "name":"github",
-          "url":"http://github.com/rbhatia46",
+          "url":"https://github.com/Nnange",
           "className":"fa fa-github"
         },
         {
           "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
+          "url":"https://www.facebook.com/nnange.awong/",
+          "className":"fa fa-facebook"
         }
       ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
+    "aboutme":"I a  5th Semester Student at Rhein-Waal University of Applied Sciences pursuing a Bsc in Communication and Information Enginnering. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Germany",
     "website":"https://rbhatia46.github.io",
     "education":[
       {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
+        "UniversityName":"Rhein-Waal University of Applied Sciences",
+        "specialization":"Communication and Information Engineering",
+        "MonthOfPassing":"Oct 2018",
+        "YearOfPassing":"Present",
+        "Achievements":"• 5th Semester"
       },
       {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
+        "UniversityName":"University of Buea",
+        "specialization":"Computer Engineering",
+        "MonthOfPassing":"Oct 2018",
+        "YearOfPassing":"- Sept 2018",
+        "Achievements":"• Stopped in the 4th Semester"
+      },
+      {
+        "UniversityName":"Saint Joseph College Sasse, Buea (Cameroon)",
+        "specialization":"Higher Education",
+        "MonthOfPassing":"Sept 2014",
+        "YearOfPassing":"- June 2016",
+        "Achievements":"• 5 GCE Advance Level Certificate (Abitur)"
+      },
+      {
+        "UniversityName":"Saint Joseph College Sasse, Buea (Cameroon)",
+        "specialization":"Secondary Education",
+        "MonthOfPassing":"Sept 2009",
+        "YearOfPassing":"- June 2014",
+        "Achievements":"• 11 GCE Ordinary Level Certificate"
       }
     ],
     "work":[
       {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
+        "CompanyName":"Institute of Geology and Mining Research",
+        "specialization":"Intern Assistant / Information Technology Assistant",
+        "MonthOfLeaving":"Jun 2016",
+        "YearOfLeaving":"– Sep 2016",
+        "Tasks1":"• Coordinating with different departments to provide support and documentation for IT processes.",
+        "Tasks2":"• Providing technological support to include hardware and software troubleshooting.",
+        "Tasks3":"• Assisting with IRGM website content maintenance.",
+        "Tasks4":"• Creating technical specifications, operating procedures and other support documentation for technology stakeholders.",
+        "Tasks5":"• Performed other IT related duties as assigned on request by the IT department."
       },
       {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
+        "CompanyName":"Njorku",
+        "specialization":"Working Student at NJORKU (Job Search Engine), Buea",
+        "MonthOfLeaving":"Mar 2017",
+        "YearOfLeaving":"– Aug 2017",
+        "Tasks1":"• Developing client websites using a variety of programming languages.",
+        "Tasks2":"• Simultaneous management of development elements of all client projects using agile methodology.",
+        "Tasks3":"• Coding front and back end solutions in a variety of languages including dynamic elements.",
+        "Tasks4":"• Maintained responsibility for the execution of routine backups and disasters recovery and partook in the installation and maintenance over company printer and equipment.",
+        "Tasks5":"• Assisted in the troubleshooting and resolution of any IOS issues on Apple Devices and as well as other devices in the company."
+      },
+      {
+        "CompanyName":"Aramdou",
+        "specialization":"Working Student at Aramdou, Buea (Cameroon)",
+        "MonthOfLeaving":"Sept 2017",
+        "YearOfLeaving":"– Jan 2018",
+        "Tasks1":"• Worked in a dynamic development team, reporting directly to creative director.",
+        "Tasks2":"• Assisted in the development of websites using several languages",
+        "Tasks3":"• Performed It related task which was assigned to me by the coordinator.",
+        "Tasks5":"• Performed regular upgrades to ensure the ongoing security and efficiency of company software and systems."
       }
     ],
     "skillsDescription":"Your skills here",
@@ -66,8 +98,14 @@ let resumeData = {
         "skillname":"CSS"
       },
       {
+        "skillname":"Javascript"
+      },
+      {
         "skillname":"Reactjs"
-      }
+      },
+      {
+        "skillname":"Java"
+      },
     ],
     "portfolio":[
       {
